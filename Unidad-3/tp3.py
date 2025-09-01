2d0e80bf739a6adf164ef1322f1f26a0183a7f91
BrunoSarmiento <brunosarmiento00@gmail.com>
2025-08-31 22:35:46 -0300

    Creacion de ramas para practicar

--- Unidad-3/tp3.py
+++ Unidad-3/tp3.py
@@ -44,3 +44,19 @@ elif edad >= 18 and edad < 30:
     print("Adulto/a joven")
 elif edad >= 30:
     print("Adulto/a")
+
+#5) Escribir un programa que permita introducir contraseñas de entre 8 y 14 caracteres
+#(incluyendo 8 y 14). Si el usuario ingresa una contraseña de longitud adecuada, imprimir por en
+#pantalla el mensaje "Ha ingresado una contraseña correcta"; en caso contrario, imprimir por
+#pantalla "Por favor, ingrese una contraseña de entre 8 y 14 caracteres". Nota: investigue el uso
+#de la función len() en Python para evaluar la cantidad de elementos que tiene un iterable tal
+#como una lista o un string.
+
+contrasenia:str = input("Ingrese una contraseña de entre 8 y 14 caracteres: ")
+
+if len(contrasenia) >= 8 and len(contrasenia) <= 14:
+    print("Ha ingresado una contraseña correcta")
+else:
+    print("Por favor ingrese una contraseña de netre 8 y 14 caracteres")
+
+
